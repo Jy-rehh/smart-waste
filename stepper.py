f3ccf6d9fd20dda365f50c5c128675eeb3e622ea
Jy-rehh <j-jireh.tejano@cit.edu>
2025-04-10 20:46:43 +0800

    asda

--- stepper.py
+++ stepper.py
@@ -1,30 +1,37 @@
 import RPi.GPIO as GPIO
 import time
 
+# Define GPIO pins
 IN1 = 17
 IN2 = 18
 IN3 = 27
 IN4 = 22
 
+# Setup GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-
 GPIO.setup(IN1, GPIO.OUT)
 GPIO.setup(IN2, GPIO.OUT)
 GPIO.setup(IN3, GPIO.OUT)
 GPIO.setup(IN4, GPIO.OUT)
 
-# Full-step sequence (less torque, but simple)
+# Half-step sequence for 28BYJ-48
 step_sequence = [
-    [1, 0, 1, 0],
+    [1, 0, 0, 1],
+    [1, 0, 0, 0],
+    [1, 1, 0, 0],
+    [0, 1, 0, 0],
     [0, 1, 1, 0],
-    [0, 1, 0, 1],
-    [1, 0, 0, 1]
+    [0, 0, 1, 0],
+    [0, 0, 1, 1],
+    [0, 0, 0, 1]
 ]
 
-def step_motor(steps, delay):
-    for _ in range(steps):
-        for step in step_sequence:
+# Function to move steps
+def move_steps(step_count, delay, direction):
+    sequence = step_sequence[::-1] if direction == "backward" else step_sequence
+    for _ in range(step_count):
+        for step in sequence:
             GPIO.output(IN1, step[0])
             GPIO.output(IN2, step[1])
             GPIO.output(IN3, step[2])
@@ -32,9 +39,30 @@ def step_motor(steps, delay):
             time.sleep(delay)
 
 try:
-    print("Rotating...")
-    step_motor(512, 0.002)  # One full rotation for 28BYJ-48
-    print("Done.")
+    print("Stepper acting like servo...")
+
+    delay = 0.0015
+    angle_90 = 128  # 90 degrees
+    angle_180 = -256  # 180 degrees
+
+    # Center is assumed to be 0 steps from boot
+
+    # Move left (−90°)
+    move_steps(angle_90, delay, "backward")
+
+    # Return to center
+    move_steps(angle_90, delay, "forward")
+
+    time.sleep(1)
+
+    # Move right (+180°)
+    move_steps(angle_180, delay, "forward")
+
+    # Return to center
+    move_steps(angle_180, delay, "backward")
+
+    print("Sweep complete.")
 
 finally:
     GPIO.cleanup()
+    print("GPIO cleaned up.")
