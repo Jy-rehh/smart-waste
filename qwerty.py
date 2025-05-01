37c1e441e1f075b458c093db7757a005dfc3c3d7
Jy-rehh <j-jireh.tejano@cit.edu>
2025-05-01 18:19:33 +0800

    gpio

--- qwerty.py
+++ qwerty.py
@@ -326,7 +326,8 @@ def set_servo_position(pos):
         last_servo_position = pos
 
 # Ultrasonic setup
-GPIO.setmode(GPIO.BCM)  # or GPIO.BOARD, depending on your preference
+if not GPIO.getmode():  # Check if mode has already been set
+    GPIO.setmode(GPIO.BCM)
 
 TRIG_PIN = 10  # pin 19
 ECHO_PIN = 9   # pin 21
