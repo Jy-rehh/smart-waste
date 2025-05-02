cc9a14853602d044e0d241a38d4fa61a0eb35e97
Jy-rehh <j-jireh.tejano@cit.edu>
2025-05-02 18:37:29 +0800

    88%

--- qwerty.py
+++ qwerty.py
@@ -385,7 +385,7 @@ try:
 
                 for box in results.boxes:
                     confidence = box.conf[0].item()
-                    if confidence >= 0.87:
+                    if confidence >= 0.88:
                         class_id = int(box.cls[0])
                         class_name = bottle_model.names[class_id].lower()
 
