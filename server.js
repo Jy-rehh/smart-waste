70884a9f39fa8bd75c3a80e5dc71acf0026f3b37
Jy-rehh <j-jireh.tejano@cit.edu>
2025-05-02 20:24:00 +0800

    sda

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const admin = require('firebase-admin');
-const functions = require('firebase-functions');
 const path = require('path');
 const { spawn } = require('child_process');
 const bodyParser = require('body-parser'); 
@@ -71,40 +70,6 @@ app.post('/start-bottle-session', async (req, res) => {
 
   res.json({ queuePosition: 1 });
 });
-/**
- * Auto delete queue
- */
-admin.initializeApp();
-exports.cleanupExpiredSessions = functions.pubsub
-  .schedule('every 1 minutes')
-  .onRun(async (context) => {
-    const now = Date.now();
-    const expirationLimit = 90 * 1000; // 90 seconds
-
-    const usersRef = db.collection('Users Collection');
-    const snapshot = await usersRef.where('queuePosition', '==', 1).get();
-
-    if (snapshot.empty) {
-      console.log('No active sessions to check.');
-      return null;
-    }
-
-    for (const doc of snapshot.docs) {
-      const data = doc.data();
-      const startedAt = data.sessionStartedAt || 0;
-      const secondsElapsed = (now - startedAt);
-
-      if (secondsElapsed >= expirationLimit) {
-        await usersRef.doc(doc.id).update({
-          queuePosition: admin.firestore.FieldValue.delete(),
-          sessionStartedAt: admin.firestore.FieldValue.delete(),
-        });
-        console.log(`Session expired and cleared for UserID: ${data.UserID}`);
-      }
-    }
-
-    return null;
-  });
 
 /**
  * Finish session — remove the user from queue and shift others
